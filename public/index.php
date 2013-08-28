f6e056073a276fa29b9db855f09651ef14efc950
williancarminato <williancarminato@gmail.com>
2013-08-28 14:21:08 -0300

    Adding About page and some more bootstrap files

--- public/index.php
+++ public/index.php
@@ -6,6 +6,10 @@ use Respect\Rest\Router;
 
 $router = new Router();
 
+$router->get('/about', function() use ($template){
+    return $template->twig->render('about.html');
+});
+
 $router->get('/', function() use ($template){
     $var = $template->twig->render('index.html', array('name'=>'Respect'));
     return $var;
